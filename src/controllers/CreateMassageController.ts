b6c8b36066ff92ffceed9a91f90a724929e31548
Maclevison Silva <maclevison@gmail.com>
2021-10-19 16:29:32 +0100

    feat: create message

---
+++ src/controllers/CreateMassageController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import { CreateMessageService } from "../services/CreateMassageService";
+
+class CreateMessageController {
+  async handle(request: Request, response: Response) {
+    const { message } = request.body;
+    const { user_id } = request;
+    const service = new CreateMessageService();
+    const result = await service.execute(message, user_id);
+
+    return response.json(result);
+  }
+}
+
+export { CreateMessageController };
